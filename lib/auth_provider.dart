bb3848f00626f934c50cb04378d82f8a23756423
AdiKK17 <kavikmr9@gmail.com>
2020-02-22 23:05:18 +0530

    ngrok updated

--- lib/auth_provider.dart
+++ lib/auth_provider.dart
@@ -19,7 +19,7 @@ class AuthProvider extends ChangeNotifier {
 
   final List<Map<String,dynamic>> _loggedInUserAccounts = [];
 
-  final ngrokUrl = "https://e3ecddb1.ngrok.i";
+  final ngrokUrl = "https://e3ecddb1.ngrok.i0";
 
 
   String get username{
